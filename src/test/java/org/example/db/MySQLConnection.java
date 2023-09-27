e98865bbefd5e112a3f64fe167dc5de21f1e6baa
Negar Baharmand <negarbaharmand@outlook.com>
2023-09-27 10:41:40 +0200

    Create Connection class

---
+++ src/test/java/org/example/db/MySQLConnection.java
@@ -0,0 +1,24 @@
+package org.example.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PWD = "negar";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/todoit";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PWD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+}
