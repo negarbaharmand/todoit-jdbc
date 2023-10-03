48090eeec2e0cb211aa001c5938d60a88ce7db00
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-03 19:50:59 +0200

    implement logic for findByDoneStatus method in TodoItemDaoImpl

--- src/main/java/org/example/dao/impl/TodoItemDaoImpl.java
+++ src/main/java/org/example/dao/impl/TodoItemDaoImpl.java
@@ -1,6 +1,7 @@
 package org.example.dao.impl;
 
 import org.example.dao.TodoItemsDao;
+import org.example.dao.impl.db.MySQLConnection;
 import org.example.model.Person;
 import org.example.model.Todo_Item;
 
@@ -8,6 +9,7 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TodoItemDaoImpl implements TodoItemsDao {
@@ -23,7 +25,7 @@ public class TodoItemDaoImpl implements TodoItemsDao {
         String query = "insert into todo_item (title, description, deadline, done, assignee_id) values (?, ?, ?, ?, ?)";
 
         try (
-             PreparedStatement preparedStatement = connection.prepareStatement(query);
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
         ) {
             preparedStatement.setString(1, item.getTitle());
             preparedStatement.setString(2, item.getDescription());
@@ -56,8 +58,8 @@ public class TodoItemDaoImpl implements TodoItemsDao {
         List<Todo_Item> items = new ArrayList<Todo_Item>();
         String query = "SELECT * from todo_item";
         try (
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("query");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("query");
         ) {
             while (resultSet.next()) {
                 int todoId = resultSet.getInt(1);
@@ -79,22 +81,22 @@ public class TodoItemDaoImpl implements TodoItemsDao {
     @Override
     public Todo_Item findById(int id) {
         String query = "select * from todo_item where id = ?";
-        try(
+        try (
                 PreparedStatement preparedStatement = connection.prepareStatement(query);
 
-                ) {
+        ) {
             preparedStatement.setInt(1, id);
-            try(ResultSet resultSet = preparedStatement.executeQuery();) {
-            if (resultSet.next()) {
-                int todoId = resultSet.getInt(1);
-                String title = resultSet.getString(2);
-                String description = resultSet.getString(3);
-                LocalDate deadline = resultSet.getDate(4).toLocalDate();
-                boolean done = resultSet.getBoolean(5);
-                int assignedId = resultSet.getInt(6);
-                return new Todo_Item(todoId, title, description, deadline, done, assignedId);
-            }
-        }catch (SQLException e) {
+            try (ResultSet resultSet = preparedStatement.executeQuery();) {
+                if (resultSet.next()) {
+                    int todoId = resultSet.getInt(1);
+                    String title = resultSet.getString(2);
+                    String description = resultSet.getString(3);
+                    LocalDate deadline = resultSet.getDate(4).toLocalDate();
+                    boolean done = resultSet.getBoolean(5);
+                    int assignedId = resultSet.getInt(6);
+                    return new Todo_Item(todoId, title, description, deadline, done, assignedId);
+                }
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         } catch (SQLException e) {
@@ -105,8 +107,33 @@ public class TodoItemDaoImpl implements TodoItemsDao {
 
     @Override
     public Collection<Todo_Item> findByDoneStatus(boolean val) {
-        return null;
+        List<Todo_Item> items = new ArrayList<>();
+        String query = "Select * from todo_item where done = ?";
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ) {
+
+            preparedStatement.setBoolean(1, val);
+            try (ResultSet resultSet = preparedStatement.executeQuery()
+            ) {
+                while (resultSet.next()) {
+                    int todoId = resultSet.getInt(1);
+                    String title = resultSet.getString(2);
+                    String description = resultSet.getString(3);
+                    LocalDate deadline = resultSet.getDate(4).toLocalDate();
+                    int assignedId = resultSet.getInt(6);
+                    Todo_Item item = new Todo_Item(todoId, title, description, deadline, val, assignedId);
+                    items.add(item);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+        return items;
     }
+
 
     @Override
     public Collection<Todo_Item> findByAssignee(Person per) {
@@ -125,6 +152,14 @@ public class TodoItemDaoImpl implements TodoItemsDao {
 
     @Override
     public boolean deleteById(int delId) {
-        return false;
+        try (Connection connection = MySQLConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("delete from todo_item where person_id = ?")) {
+            preparedStatement.setInt(1, delId);
+            int rowsDeleted = preparedStatement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
